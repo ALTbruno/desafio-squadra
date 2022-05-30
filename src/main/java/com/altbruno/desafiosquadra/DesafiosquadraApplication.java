c8a405525179fbdc9fb1d912ac56c21b5f62c2b9
Bruno Barbosa <brb.dev@outlook.com>
2022-05-29 21:22:14 -0400

    first commit

---
+++ src/main/java/com/altbruno/desafiosquadra/DesafiosquadraApplication.java
@@ -0,0 +1,13 @@
+package com.altbruno.desafiosquadra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiosquadraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiosquadraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altbruno/desafiosquadra/DesafiosquadraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altbruno.desafiosquadra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiosquadraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
